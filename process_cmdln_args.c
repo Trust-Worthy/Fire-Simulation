3f1f22f76ebe072da8b23d5c9de1f76d56a5e987
Jonathan M Bateman <jmb7342@rit.edu>
2024-10-20 22:30:53 -0400

    called print_struct from main

--- process_cmdln_args.c
+++ process_cmdln_args.c
@@ -32,12 +32,11 @@ void print_help_message(){
     "By default, the simulation runs in overlay display mode.\n"
     "The -pN option makes the simulation run in print mode for up to N states.\n"
     "\n"
-    "\n"
     "Simulation Configuration Options:\n"
     "-H  # View simulation options and quit.\n"
     "-bN # proportion of trees that are already burning. 0 < N < 101.\n"
-    " -cN # probability that a tree will catch fire. 0 < N < 101.\n"
-    " -dN # density: the proportion of trees in the grid. 0 < N < 101.\n"
+    "-cN # probability that a tree will catch fire. 0 < N < 101.\n"
+    "-dN # density: the proportion of trees in the grid. 0 < N < 101.\n"
     "-nN # proportion of neighbors that influence a tree catching fire. -1 < N < 101.\n"
     "-pN # number of states to print before quitting. -1 < N < ...\n"
     "-sN # simulation grid size. 4 < N < 41.\n");
@@ -158,7 +157,7 @@ int main( int argc, char * argv[] ) {
     // use cmd ln struct to initialize the simulation
 
 
-
+    print_struct(&cmds_struct);
 
 
     
